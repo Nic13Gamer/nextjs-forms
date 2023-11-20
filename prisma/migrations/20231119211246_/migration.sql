481ace2c62ad305898942e9316956b579ceba903
Nic13Gamer <54724556+Nic13Gamer@users.noreply.github.com>
2023-11-20 15:47:31 -0300

    move prisma db to supabase

--- prisma/migrations/20231119211246_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Form" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "questions" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
